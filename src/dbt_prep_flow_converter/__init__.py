4266176e9da73130b18de3f283b4ffaaf758699c
Vince Faller <vince.faller@lumentum.com>
2025-04-25 14:26:07 -0700

    got something at least working

---
+++ src/dbt_prep_flow_converter/__init__.py
@@ -0,0 +1,3 @@
+from dbt_prep_flow_converter.convert import run, run_cli
+
+__all__ = ["run", "run_cli"]
--- tests/test_foo.py
+++
@@ -1,5 +0,0 @@
-from dbt_prep_flow_converter.foo import foo
-
-
-def test_foo():
-    assert foo("foo") == "foo"
